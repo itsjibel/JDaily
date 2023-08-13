2d8b71d563c7530cda2cce858d393298366289f6
Jibel <itsjibel@gmail.com>
2023-08-13 14:40:12 +0330

    Start implement graphical UI

--- src/jdaily.py
+++ src/jdaily.py
@@ -1,1 +1,18 @@
-print("Welcome to JDaily")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+def window():
+   app = QApplication(sys.argv)
+   widget = QWidget()
+
+   textLabel = QLabel(widget)
+   textLabel.setText("Welcome to JDaily!")
+   textLabel.move(95, 240)
+
+   widget.setGeometry(50, 50, 320, 520)
+   widget.setWindowTitle("JDaily")
+   widget.show()
+   sys.exit(app.exec_())
+
+if __name__ == '__main__':
+   window()
